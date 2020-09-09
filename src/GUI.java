814e42c77746e5356f22c3f5920e09842c2888eb
Michael Wallerius <mikesterciv1@gmail.com>
2020-09-09 14:44:11 -0700

    buttons

--- src/GUI.java
+++ src/GUI.java
@@ -13,34 +13,50 @@ public class GUI implements ActionListener{
 	
 	JFrame frame;
 	JButton button;
+	JButton button2;
+	JButton button3;
+	JButton button4;
 	JPanel panel;
 	
 	public GUI() {
 		frame = new JFrame();
-		button = new JButton();
-		panel = new JPanel();
+		button = new JButton("Show Teachers");
+		button2 = new JButton("Show Police Officers");
+		button3 = new JButton("Show Engineers");
+		button4 = new JButton("Show Bankers");
+		
+		panel = new JPanel(new GridLayout(0, 1));
 		
 		panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,10));
-		panel.setLayout(new GridLayout(0, 1));
-		//panel.add(button);
-		//button.addActionListener(this);
-		
 		frame.add(panel,BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Employee List");
 		frame.pack();
 		frame.setVisible(true);
+		
+		panel.add(button);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		
+		button.addActionListener(this);
+		button2.addActionListener(this);
 	}
 	
 	public void displayElement(String x) {
 		JLabel label = new JLabel(x);
+		label.setBounds(0, 0, 200, 50);
 		panel.add(label);
 	}
 	
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		JLabel label = new JLabel("Hello");
+		panel.add(label);
+		panel.revalidate();
+		panel.repaint();
+		System.out.println("Hi");
 		
 	}
 }
